b708900d69f4d63d2dc0c051b7dd893f9a76c993
MANUEL-IONUȚ MARTIN <manuel.martin@stud.ubbcluj.ro>
2021-11-14 13:07:56 +0200

    Modificari

---
+++ Doughnut.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Martin_Manuel_Lab2
+{
+    class Doughnut
+    {
+    }
+}
+public enum DoughnutType
+{
+    Glazed,
+    Sugar,
+    Lemon,
+    Chocolate,
+    Vanilla
+}
+class Doughnut
+{
+    private DoughnutType mFlavor;
+
+    public DoughnutType Flavor
+    {
+        get
+        {
+            return mFlavor;
+        }
+        set
+        {
+            mFlavor = value;
+        }
+    }
+    private float mPrice;
+    public float Price
+    {
+        get
+        {
+            return mPrice;
+        }
+        set
+        {
+            mPrice = value;
+        }
+    }
+    public Doughnut(DoughnutType aFlavor) // constructor
+    {
+        mFlavor = aFlavor;
+    }
+}
+
+
